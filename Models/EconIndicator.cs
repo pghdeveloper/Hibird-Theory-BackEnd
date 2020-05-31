d2590405f366e45baa6cf9fc7f3da16308dd4f2f
pghdeveloper <34989427+pghdeveloper@users.noreply.github.com>
2020-05-31 13:42:17 -0400

    lower case the model properties
    lower case the model properties

--- Models/EconIndicator.cs
+++ Models/EconIndicator.cs
@@ -3,11 +3,11 @@ namespace Hibird_Theory_Backend.Models
 {
     public class EconIndicator
     {
-        public string sectorName {get; set;}
-        public float CO2 {get; set;}
-        public float GDP {get; set;}
-        public float Employment {get; set;} //set to "BeforeCovid" or "AfterCovid"
-        public float Fossil {get; set;}
-        public float NonFossil {get; set;}
+        public string sectorname {get; set;}
+        public float co2 {get; set;}
+        public float gdp {get; set;}
+        public float employment {get; set;} //set to "BeforeCovid" or "AfterCovid"
+        public float fossil {get; set;}
+        public float nonfossil {get; set;}
     }
 }
