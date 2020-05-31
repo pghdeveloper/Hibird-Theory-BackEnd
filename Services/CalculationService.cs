767b647b272b32ca163d9501d55ed052e48dec6a
pghdeveloper <34989427+pghdeveloper@users.noreply.github.com>
2020-05-31 17:24:53 -0400

    Pushing more
    Pushing more

--- Services/CalculationService.cs
+++ Services/CalculationService.cs
@@ -16,6 +16,23 @@ namespace Hibird_Theory_Backend.Services
         {
             //calculatione 5
             //convert pizza committments to dollar amount
+            var dollarAmount = pizzaCommitments * 15;
+
+            // read demand vector (86 values)
+            // add dolarAmount to demand[i], where i is index of foor service
+            // add 5 dollar (deliveary) to demand[j], j is index of "Other informat"
+            // as result updated demand
+            // persist updare demand
+
+            // multiply (matrix operation) leonmatrix (86x86) by demand vector (86)
+            // result leonresult vector (86)
+
+            // multiply (loop though each value) leonresult  (86) by coeff (86) to get indicators
+            // result new dynamicwithcommitments.json
+            // persist dynamicwithcommitments.json
+
+            // return dynamicwithcommitments.json
+
             //Take into account of 5 dollar fixed price for each committment
             //the total amount of the pizza will be to the final demand
             //5 dollar to Other Intenet services
